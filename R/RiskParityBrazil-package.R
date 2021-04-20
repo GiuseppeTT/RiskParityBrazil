a97c0f91afd722cf95f505903bd7b3cdcb550792
GiuseppeTT <giu.tinti@gmail.com>
2021-04-20 19:15:26 -0300

    Add R package structure

---
+++ R/RiskParityBrazil-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom glue glue
+## usethis namespace: end
+NULL
+
+# The following commands prevent some R check complaints
+utils::globalVariables(".")
