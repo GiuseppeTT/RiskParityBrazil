b3f0637d27063bc6f5afe001752988d7da7684de
GiuseppeTT <67562667+GiuseppeTT@users.noreply.github.com>
2021-04-29 17:44:53 +0000

    Deploying to gh-pages from @ GiuseppeTT/RiskParityBrazil@e921a4a0be70cd960280ec1e12dce4662797ea36 🚀

--- R/utils-zero-first.R
+++
@@ -1,7 +0,0 @@
-zero_first <- function(
-    x
-) {
-    x %>%
-        magrittr::inset2(1, 0) %>%
-        return()
-}
--- R/utils-zero-na.R
+++
@@ -1,7 +0,0 @@
-zero_na <- function(
-    x
-) {
-    x %>%
-        tidyr::replace_na(0) %>%
-        return()
-}
